50e628994c0a10b9b39c6bf16ebda4265e594ba7
Nicholas Bacon <mordecailloyd@gmail.com>
2017-02-16 19:10:35 -0800

    error message / -1 return

--- altconcat.py
+++ altconcat.py
@@ -10,6 +10,10 @@ def linear_search(list, element):
 linear_search([0,1,4,'a',-4,[7,'a',-4,5,90]],7)
 
 
+#def bin_search(find, items )
+
+
+
 def bin_search(find, items, low=0, high=None):
 	if high is None:
 		high= len(items)
@@ -17,15 +21,16 @@ def bin_search(find, items, low=0, high=None):
 		high = high
 	position = low + (high - low) / len(items)
 #    position = (high - low) // 2 + low
-	if position =len(items):
-		return False
-    if low == high:
-        return False
-    if find == items[position]:
-        return position
-    elif find > items[position]:
-    	position+=1
-        return bin_search(items, find, low=(position), high)
-    elif find < items[position]:
-        return bin_search(items, find, low=low, high=position)
+	if position ==len(items):
+		return -1
+	if low == high:
+		return -1
+	if find == items[position]:
+		return position
+	elif find > items[position]:
+		position+=1
+		return bin_search(find, items, position, high)
+	elif find < items[position]:
+		return bin_search(find, items, low, position)
 
+print bin_search(-1,[-9,0,1,2,3,4,5,9])
